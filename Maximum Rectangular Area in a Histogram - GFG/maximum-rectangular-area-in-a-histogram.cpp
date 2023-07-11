4c0b051cce64f8e93eebda3ba60a7a6c0f96b041
Rishabh Shukla <110485006+rishabh-i13@users.noreply.github.com>
2023-07-11 14:07:20 +0530

    Added solution - LeetHub

--- Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
+++ Maximum Rectangular Area in a Histogram - GFG/maximum-rectangular-area-in-a-histogram.cpp
@@ -8,86 +8,46 @@ class Solution
 {
     public:
     //Function to find largest rectangular area possible in a given histogram.
-    long long getMaxArea(long long heights[], int n)
+    long long getMaxArea(long long arr[], int n)
     {
-        // stack<int> first;
-        // vector<int> ps;
-        // for(int i=0;i<n;i++){
-        //     while(first.empty()==0 and arr[i]<=arr[first.top()]){
-        //         first.pop();
-        //     }
-        //     if(first.empty()==1){
-        //         ps.push_back(-1);
-        //     }
-        //     else{
-        //         ps.push_back(first.top());
-        //     }
-        //     first.push(i);
-        // }
-        
-        // stack<int> second;
-        // vector<int> ns;
-        // for(int i=n-1;i>=0;i--){
-        //     while(second.empty()==0 and arr[i]<=arr[second.top()]){
-        //         second.pop();
-        //     }
-        //     if(second.empty()==1){
-        //         ns.push_back(-1);
-        //     }
-        //     else{
-        //         ns.push_back(second.top());
-        //     }
-        //     second.push(i);
-        // }
-        // reverse(ns.begin(),ns.end());
-        // long long maxArea=-1;
-        // for(int i=0;i<n;i++){
-        //     long long temp=(ns[i]-ps[i]-1)*arr[i];
-        //     maxArea=max(maxArea,temp);
-        // }
-        // return maxArea;
-        // Your code here
-        
-         vector<int> ps;
-        stack<int> st1;
-        
-        //for prev smaller
+        stack<int> first;
+        vector<int> ps;
         for(int i=0;i<n;i++){
-            while(st1.empty()==0 and heights[i]<=heights[st1.top()]){
-                st1.pop();
+            while(first.empty()==0 and arr[i]<=arr[first.top()]){
+                first.pop();
             }
-            if(st1.empty()==1){
+            if(first.empty()==1){
                 ps.push_back(-1);
             }
             else{
-                ps.push_back(st1.top());
+                ps.push_back(first.top());
             }
-            st1.push(i);
+            first.push(i);
         }
-       
+        
+        stack<int> second;
         vector<int> ns;
-        stack<int> st2;
-        //next smaller
         for(int i=n-1;i>=0;i--){
-            while(st2.empty()==0 and heights[i]<=heights[st2.top()]){
-                st2.pop();
+            while(second.empty()==0 and arr[i]<=arr[second.top()]){
+                second.pop();
             }
-            if(st2.empty()==1){
+            if(second.empty()==1){
                 ns.push_back(n);
             }
             else{
-                ns.push_back(st2.top());
+                ns.push_back(second.top());
             }
-            st2.push(i);
+            second.push(i);
         }
         reverse(ns.begin(),ns.end());
-        long long  maxarea=-1;
+        long long maxArea=-1;
         for(int i=0;i<n;i++){
-            long long  x=(ns[i]-ps[i]-1)*heights[i];
-            maxarea=max(maxarea,x);
+            long long temp=(ns[i]-ps[i]-1)*arr[i];
+            maxArea=max(maxArea,temp);
         }
-
-        return maxarea;
+        return maxArea;
+        // Your code here
+      
     }
 };
 
